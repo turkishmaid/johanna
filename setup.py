aee4d7242b9804b99203e5282daa0c040ed6ae56
Sara Ziner <sara.ziner@googlemail.com>
2020-08-28 23:01:41 +0200

    Fix release number to v0.1.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='johanna',
-      version='0.1',
+      version='v0.1.0',
       description='Sweet tiny app framework for Jenkins driven background apps',
       url='https://github.com/turkishmaid/johanna',
       author='Sara Ziner',
